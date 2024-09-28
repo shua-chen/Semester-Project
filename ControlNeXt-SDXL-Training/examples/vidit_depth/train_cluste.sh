b3e44163f4341f638c604e542ea205f082dba77e
shua-chen <shua.chen6666@gmail.com>
2024-09-28 21:55:13 +0200

    add train sh file for ic cluster

---
+++ ControlNeXt-SDXL-Training/examples/vidit_depth/train_cluste.sh
@@ -0,0 +1,20 @@
+accelerate launch train_controlnext.py --pretrained_model_name_or_path "stabilityai/stable-diffusion-xl-base-1.0" \
+--pretrained_vae_model_name_or_path "madebyollin/sdxl-vae-fp16-fix" \
+--variant fp16 \
+--use_safetensors \
+--output_dir "train/example" \
+--logging_dir "logs" \
+--resolution 512 \
+--gradient_checkpointing \
+--set_grads_to_none \
+--proportion_empty_prompts 0.2 \
+--controlnet_scale_factor 1.0 \
+--mixed_precision fp16 \
+--enable_xformers_memory_efficient_attention \
+--train_data_dir "/scratch/students/2024-fall-shuhua/dataset/FFHQ/512/01000" \
+--train_data_opt "/scratch/students/2024-fall-shuhua/code/Semester-Project/data_opt.yaml" \
+--image_column "image" \
+--conditioning_image_column "depth_map" \
+--caption_column "caption" \
+--validation_prompt "a high resolution human face image" \
+--validation_image "examples/vidit_depth/condition_0.png" \
